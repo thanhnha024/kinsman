0770efe76f4e39698ef700c7adb307722b18ed5e
shin <nhathanh024@gmail.com>
2024-05-07 09:52:37 +0700

    change theme name

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var nameFolderTheme = "shin-child";
+var nameFolderTheme = "hello-elementor-child";
 var gulp = require("gulp");
 var destFolder = "src/wp-content/themes/" + nameFolderTheme + "/assets/main";
 
